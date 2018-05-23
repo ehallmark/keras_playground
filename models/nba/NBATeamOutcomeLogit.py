fd09c1362a74a44bd4a2a221ea872b4ecbd55168
Evan Hallmark <ehallmark@gttgrp.com>
2018-05-23 15:44:32 -0700

    c

--- models/nba/NBATeamOutcomeLogit.py
+++ models/nba/NBATeamOutcomeLogit.py
@@ -7,70 +7,86 @@ import matplotlib.pyplot as plt
 
 '''
 
+
+def test_model(model, x, y):
+    predictions = model.predict(x)
+    binary_predictions = (predictions >= 0.5).astype(int)
+    binary_errors = np.array(y) != np.array(binary_predictions)
+    binary_errors = binary_errors.astype(int)
+    errors = np.array(y - np.array(predictions))
+    binary_correct = predictions.shape[0] - int(binary_errors.sum())
+    binary_percent = float(binary_correct) / predictions.shape[0]
+    avg_error = np.mean(np.abs(errors), -1)
+    return binary_correct, y.shape[0], binary_percent, avg_error
+
+
+def to_percentage(x):
+    return str("%0.2f" % (x * 100))+'%'
+
+
 def bool_to_int(b):
     if b:
         return 1.0
     else:
         return 0.0
 
-#team_id = '1610612739'  # cleveland  '1610612757' # portland
+
 conn = create_engine("postgresql://localhost/ib_db?user=postgres&password=password")
 sql = pd.read_sql('select * from nba_games_all where game_date is not null and ' +
                   'season_type = \'Regular Season\' and h_fg3_pct is ' +
-                  'not null and a_fg3_pct is not null '+
-                 # 'and \''+ team_id+'\'= ANY(ARRAY[h_team_id]) ' +
-                 # 'and season_year >= 2010 ' +
+                  'not null and a_fg3_pct is not null and a_stl is not null and h_oreb is not null '+
                   'order by game_date asc', conn)
 
 test_season = 2017
 
-sql['spread'] = sql['h_pts'] - sql['a_pts']
-sql['total'] = sql['h_pts'] + sql['a_pts']
+sql['spread'] = sql['h_pts'].astype(np.float64) - sql['a_pts'].astype(np.float64)
+sql['total'] = sql['h_pts'].astype(np.float64) + sql['a_pts'].astype(np.float64)
 sql['y'] = [bool_to_int(y >= 0) for y in sql['spread']]
-sql['stl'] = sql['h_stl'] - sql['a_stl']
-sql['oreb'] = sql['h_oreb'] - sql['a_oreb']
-sql['tov'] = sql['h_tov'] - sql['a_tov']
-sql['fg_pct'] = sql['h_fg_pct'] - sql['a_fg_pct']
-sql['fg3m'] = sql['h_fg3m'] - sql['a_fg3m']
-sql['fta'] = sql['h_fta'] - sql['a_fta']
-sql['pf'] = sql['h_pf'] - sql['a_pf']
-sql['total_pf'] = sql['h_pf'] + sql['a_pf']
+sql['stl'] = sql['h_stl'].astype(np.float64) - sql['a_stl'].astype(np.float64)
+sql['oreb'] = sql['h_oreb'].astype(np.float64) - sql['a_oreb'].astype(np.float64)
+sql['tov'] = sql['h_tov'].astype(np.float64) - sql['a_tov'].astype(np.float64)
+sql['fg_pct'] = sql['h_fg_pct'].astype(np.float64) - sql['a_fg_pct'].astype(np.float64)
+sql['fg3m'] = sql['h_fg3m'].astype(np.float64) - sql['a_fg3m'].astype(np.float64)
+sql['fg3_pct'] = sql['h_fg3_pct'].astype(np.float64) - sql['a_fg3_pct'].astype(np.float64)
+sql['fta'] = sql['h_fta'].astype(np.float64) - sql['a_fta'].astype(np.float64)
+sql['pf'] = sql['h_pf'].astype(np.float64) - sql['a_pf'].astype(np.float64)
+sql['ast'] = sql['h_ast'].astype(np.float64) - sql['a_ast'].astype(np.float64)
+sql['fg3a'] = sql['h_fg3a'].astype(np.float64) - sql['a_fg3a'].astype(np.float64)
+sql['fga'] = sql['h_fga'].astype(np.float64) - sql['a_fga'].astype(np.float64)
+sql['fgm'] = sql['h_fgm'].astype(np.float64) - sql['a_fgm'].astype(np.float64)
+sql['ft_pct'] = sql['h_ft_pct'].astype(np.float64) - sql['a_ft_pct'].astype(np.float64)
 
-#total = []
-#spread = []
-#outcome = []
-#for i in range(len(sql)):
-#    if i > 0:
-#        total.append(sql['total'][i])
-#        spread.append(sql['spread'][i])
-#        outcome.append(sql['y'][i])
-#sql = sql[:-1]
-#sql['spread'] = spread
-#sql['total'] = total
-#sql['y'] = outcome
+# fill in nans and infs
+sql[sql.stl == np.nan] = 1.0
+sql[sql.stl == np.inf] = 1.0
+sql[sql.fg3m == np.nan] = 1.0
+sql[sql.fg3m == np.inf] = 1.0
+
+input_attributes = [
+    # 'h_stl', 'h_oreb', 'h_tov', 'h_fgm', 'h_fga', 'h_fg3a', 'h_fg3m', 'h_pf',
+    # 'a_stl', 'a_oreb', 'a_tov', 'a_fgm', 'a_fga', 'a_fg3a', 'a_fg3m', 'a_fta'
+    'stl', 'oreb', 'tov', 'fgm', 'fga', 'ast', 'fg3a', 'fg3m', 'a_pf'  # How big of an issue is quasi-separation???
+]
+all_attributes = list(input_attributes)
+all_attributes.append('y')
+all_attributes.append('season_year')
+
+sql = sql[all_attributes].astype(np.float64)
 
 test_data = sql[sql.season_year == test_season]
 sql = sql[sql.season_year != test_season]
-input_attributes = [
-    #'h_stl', 'h_oreb', 'h_tov', 'h_fg_pct', 'h_fg3m', 'h_fta',
-    #'a_stl', 'a_oreb', 'a_tov', 'a_fg_pct', 'a_fg3m', 'a_fta'
-    'stl', 'oreb', 'tov', 'fg_pct', 'fg3m', 'a_pf'
-]
+
+print('Attrs: ', sql[input_attributes])
 
 # model to predict the total score (h_pts + a_pts)
 results = smf.logit('y ~ '+'+'.join(input_attributes), data=sql).fit()
 print(results.summary())
 
-predictions = results.predict(test_data)
-binary_predictions = (predictions >= 0.5).astype(int)
-binary_errors = np.array(test_data['y']) != np.array(binary_predictions)
-binary_errors = binary_errors.astype(int)
-errors = np.array(test_data['y'] - np.array(predictions))
-binary_correct = predictions.shape[0]-int(binary_errors.sum())
-binary_percent = float(binary_correct)/predictions.shape[0]
-print('Correctly predicted: '+str(binary_correct)+' out of '+str(predictions.shape[0]) +
-      ' ('+str("%0.2f" % (binary_percent*100))+'%)')
-print('Average error: ', np.mean(np.abs(errors), -1))
+binary_correct, n, binary_percent, avg_error = test_model(results, test_data, test_data['y'])
+
+print('Correctly predicted: '+str(binary_correct)+' out of '+str(n) +
+      ' ('+to_percentage(binary_percent)+')')
+print('Average error: ', to_percentage(avg_error))
 
 exit(0)
 
