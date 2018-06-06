fb2531705c6d75a6eab86beff5bb9e63bd328e05
Evan Hallmark <ehallmark@gttgrp.com>
2018-06-06 10:35:42 -0700

    c

--- models/nba/NBAPlayerOutcomeLogit.py
+++ models/nba/NBAPlayerOutcomeLogit.py
@@ -35,16 +35,15 @@ def bool_to_int(b):
 
 def get_all_data(attributes, test_season=2017, start_year=2003):
     conn = create_engine("postgresql://localhost/ib_db?user=postgres&password=password")
-    sql = pd.read_sql('''
-    select p1.*,p2.w as w0, p2.l as l0, p2.plus_minus::double precision/greatest(1,p2.num_games_played) as plus_minus0, p2.fg_pct as fg_pct0, p2.dreb as dreb0, p2.blk as blk0, p2.ast as ast0, p2.pf as pf0,p2.min as min0, p2.fta as fta0, p2.tov as tov0, p2.pts as pts0, p2.oreb as oreb0, p2.fga as fga0, p2.fgm as fgm0, 
+    sql = ''' select p1.*,p2.w as w0, p2.l as l0, p2.plus_minus::double precision/greatest(1,p2.num_games_played) as plus_minus0, p2.fg_pct as fg_pct0, p2.dreb as dreb0, p2.blk as blk0, p2.ast as ast0, p2.pf as pf0,p2.min as min0, p2.fta as fta0, p2.tov as tov0, p2.pts as pts0, p2.oreb as oreb0, p2.fga as fga0, p2.fgm as fgm0, 
                       p3.w as w1, p3.l as l1, p3.plus_minus::double precision/greatest(1,p3.num_games_played) as plus_minus1, p3.fg_pct as fg_pct1, p3.dreb as dreb1, p3.blk as blk1, p3.ast as ast1, p3.pf as pf1, p3.min as min1, p3.fta as fta1, p3.tov as tov1, p3.pts as pts1, p3.oreb as oreb1, p3.fga as fga1, p3.fgm as fgm1, 
                       p4.w as w2, p4.l as l2, p4.plus_minus::double precision/greatest(1,p4.num_games_played) as plus_minus2, p4.fg_pct as fg_pct2, p4.dreb as dreb2, p4.blk as blk2, p4.ast as ast2, p4.pf as pf2, p4.min as min2, p4.fta as fta2, p4.tov as tov2, p4.pts as pts2, p4.oreb as oreb2, p4.fga as fga2, p4.fgm as fgm2, 
                       p5.plus_minus as plus_minus3, p5.fg_pct as fg_pct3, p5.dreb as dreb3, p5.blk as blk3, p5.ast as ast3, p5.pf as pf3, p5.min as min3, p5.fta as fta3, p5.tov as tov3, p5.pts as pts3, p5.oreb as oreb3, p5.fga as fga3, p5.fgm as fgm3, 
                       player.height as height0,
                       player.weight as weight0,
-                      case when player.position ilike \'%guard%\' then 1.0 else 0.0 end as guard,
-                      case when player.position ilike \'%forward%\' then 1.0 else 0.0 end as forward,
-                      case when player.position ilike \'%center%\' then 1.0 else 0.0 end as center,
+                      case when player.position like \'%%Guard%%\' then 1.0 else 0.0 end as guard,
+                      case when player.position like \'%%Forward%%\' then 1.0 else 0.0 end as forward,
+                      case when player.position like \'%%Center%%\' then 1.0 else 0.0 end as center,
                       extract (year from p1.game_date) - extract(year from player.birth_date) as exp0
                       from nba_players_game_stats as p1 
                       left join nba_players_game_stats_month_lag as p2 
@@ -63,7 +62,9 @@ def get_all_data(attributes, test_season=2017, start_year=2003):
                       p1.stl is not null and p1.oreb is not null and p1.plus_minus is not null and
                       coalesce(p2.min,0) > 10
                       order by p1.game_date asc
-    '''.replace('{{START}}',str(start_year)).replace('{{END}}',str(test_season)), conn)
+    '''.replace('{{START}}',str(start_year)).replace('{{END}}',str(test_season))
+    print('SQL: ', sql)
+    sql = pd.read_sql(sql, conn)
 
     wins = [bool_to_int(y == 'W') for y in sql['wl']]
     print("Num wins: " + str(len(wins)))
