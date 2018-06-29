6a4d1381c189d921169c67733c0597e6768dd3e0
Evan Hallmark <ehallmark@gttgrp.com>
2018-06-29 09:05:06 -0700

    c

--- models/atp_tennis/TennisMatchSpreadSklearnModels.py
+++ models/atp_tennis/TennisMatchSpreadSklearnModels.py
@@ -180,7 +180,7 @@ def load_data(start_year, test_year, num_test_years, test_tournament=None, model
     return data, test_data
 
 
-alpha = 0.85
+alpha = 0.90
 def bet_func(epsilon):
     def bet_func_helper(price, odds, spread, prediction, row):
         spread_prob = probability_beat(spread, row['grand_slam'] > 0.5)
