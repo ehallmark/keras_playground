d35defba980ecea6f2332b62db1a4da39f697e1c
Evan Hallmark <ehallmark@gttgrp.com>
2018-07-04 14:28:17 -0700

    c

--- models/atp_tennis/Predict.py
+++ models/atp_tennis/Predict.py
@@ -7,8 +7,12 @@ import models.atp_tennis.TennisMatchSpreadSklearnModels as tennis_model
 from models.atp_tennis.TennisMatchOutcomeSklearnModels import load_spread_model, load_outcome_model
 from models.atp_tennis.TennisMatchOutcomeNN import test_model,to_percentage
 
-historical_model = load_outcome_model('Logistic')
-historical_spread_model = load_spread_model('Linear')
+historical_model = load_outcome_model('Logistic0')
+historical_spread_model = load_spread_model('Linear0')
+
+historical_model_slam = load_outcome_model('Logistic1')
+historical_spread_model_slam = load_spread_model('Linear1')
+
 future_matches_only = False
 test_year = 2018  # IMPORTANT!!
 tournaments = ['wimbledon']
@@ -16,7 +20,8 @@ tournaments = ['wimbledon']
 for tournament in tournaments:
     print("Tournament: ", tournament)
     data, test_data = tennis_model.load_data(start_year=tennis_model.start_year, num_test_years=1, test_year=test_year,
-                                test_tournament=tournament, model=historical_model, spread_model=historical_spread_model)
+                                test_tournament=tournament, model=historical_model, spread_model=historical_spread_model,
+                                slam_model=historical_model_slam, slam_spread_model=historical_spread_model_slam)
     #print('Test data: ', test_data[0:10])
     print('Test data shape: ', test_data.shape)
 
