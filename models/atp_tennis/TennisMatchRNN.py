bc6902d2a629f737a0560396a73c80b09fc25000
ehallmark <ehallmark@gttgrp.com>
2018-08-04 11:05:52 -0700

    c

--- models/atp_tennis/TennisMatchRNN.py
+++ models/atp_tennis/TennisMatchRNN.py
@@ -6,6 +6,7 @@ from models.atp_tennis.TennisMatchOutcomeLogit import load_data, to_percentage
 import models.atp_tennis.TennisMatchOutcomeLogit as tennis_model
 import numpy as np
 import pandas as pd
+import datetime
 np.random.seed(23952)
 
 
@@ -59,73 +60,79 @@ for meta in meta_attributes:
 
 
 if __name__ == '__main__':
-    data = load_data(all_attributes, test_season='2016-01-01', start_year='1990-01-01', masters_min=99)
-    test_data = load_data(all_attributes, test_season='2017-12-31', start_year='2016-01-01', masters_min=99)
+    test_date = datetime.date(2016, 1, 1)
+    end_date = datetime.date(2017, 1, 1)
+    data = load_data(all_attributes, test_season=end_date.strftime('%Y-%m-%d'), start_year='1990-01-01', masters_min=99)
+
     # data = data[data.tournament_rank < 101]
     # test_data = test_data[test_data.tournament_rank < 101]
+
     max_len = 64
     samples = 100000
     test_samples = 15000
 
     all_players = list(set(data['player_id']))
-    test_players = list(set(test_data['player_id']))
 
     data.sort_values(by=['player_id', 'start_date', 'tournament', 'round_num'],
                      inplace=True, ascending=True, kind='mergesort')
-    test_data.sort_values(by=['player_id', 'start_date', 'tournament', 'round_num'],
-                          inplace=True, ascending=True, kind='mergesort')
-    data.set_index(['player_id'], inplace=True)
-    test_data.set_index(['player_id'], inplace=True)
-    # print("Federer test data sorted: ", test_data)
 
+    data.set_index(['player_id'], inplace=True)
+    all_matches = data[['player_id', 'opponent_id', 'tournament', 'start_date']].iloc[:]
     data_grouped = data.groupby(by=['player_id'], sort=True)
-    test_data_grouped = test_data.groupby(by=['player_id'], sort=True)
 
     x = np.zeros((samples, len(input_attributes), max_len))
+    x2 = np.zeros((samples, len(input_attributes), max_len))
     x_test = np.zeros((test_samples, len(input_attributes), max_len))
+    x2_test = np.zeros((test_samples, len(input_attributes), max_len))
     y = []
     y_test = []
-    for n, test in [(samples, False), (test_samples, True)]:
-        cnt = 0
-        indices = list(range(n))
-        np.random.shuffle(indices)
-        while len(indices) > 0:
-            sample = None
-            while sample is None or sample.shape[0] < 2:
-                if test:
-                    player = test_players[np.random.randint(0, len(test_players))]
-                    sample = test_data_grouped.get_group(player)
-                else:
-                    player = all_players[np.random.randint(0, len(all_players))]
-                    sample = data_grouped.get_group(player)
+    cnt = 0
+    indices = list(range(samples))
+    test_indices = list(range(test_samples))
+    np.random.shuffle(indices)
+    np.random.shuffle(test_indices)
+    while len(indices) > 0:
+        sample = None
+        while sample is None or sample.shape[0] < 2:
+            match_row = all_matches.iloc[np.random.randint(0,all_matches.shape[0])]
+            player = all_matches['player_id']
+            sample = data_grouped.get_group(player)
 
-            l = min([max_len, sample.shape[0]-1])
-            if l == sample.shape[0]-1:
-                start_indices = [0]
-            else:
-                start_indices = []
-                for j in range(1+int(np.log(1+sample.shape[0]-l))):
-                    start_indices.append(np.random.randint(0, sample.shape[0]-1 - l))
-            for start_idx in start_indices:
-                if len(indices)==0:
+        n = max(1,sample.shape[0]-1-max_len)
+        for rowIdx in range(n):
+            rowIdx = n - 1 - rowIdx
+            if len(indices)==0:
+                break
+            if cnt % 1000 == 999:
+                print('Sample', cnt, 'out of', samples)
+            cnt = cnt + 1
+            last_idx = 0
+            row = sample.iloc[rowIdx]
+            opponent = row['opponent_id']
+            opp_sample = data_grouped.get_group(opponent)
+            opp_sample = opp_sample[((opp_sample.start_date<row['start_date'])|((opp_sample.start_date==row['start_date'])&(opp_sample.tournament==row['tournament'])&(opp_sample.round_num<row['round_num'])))]
+            test = row['start_date'] >= test_date
+            if test:
+                if len(test_indices)==0:
                     break
-                if cnt % 1000 == 999:
-                    print('Sample', cnt, 'out of', n)
-                cnt = cnt + 1
-                last_idx = 0
-                i = indices[0]
-                indices = indices[1:]
-                for j in range(l):
-                    last_idx = start_idx + j
-                    if test:
-                        x_test[i, :, j] = np.array(sample.iloc[last_idx][input_attributes])
-                    else:
-                        x[i, :, j] = np.array(sample.iloc[last_idx][input_attributes])
+                i = test_indices.pop()
+            else:
+                i = indices.pop()
 
+            for j in range(max_len):
+                last_idx = rowIdx - j
+                opp_index = opp_sample.shape[0]-1-j
                 if test:
-                    y_test.append(float(sample['y'].iloc[last_idx+1]))
+                    x_test[i, :, max_len - 1 - j] = np.array(sample.iloc[last_idx][input_attributes])
+                    x2_test[i, :, max_len - 1 - j] = np.array(opp_sample.iloc[opp_index][input_attributes])
                 else:
-                    y.append(float(sample['y'].iloc[last_idx+1]))
+                    x[i, :, max_len - 1 - j] = np.array(sample.iloc[last_idx][input_attributes])
+                    x2[i, :, max_len - 1 - j] = np.array(opp_sample.iloc[opp_index][input_attributes])
+
+            if test:
+                y_test.append(float(sample['y'].iloc[rowIdx+1]))
+            else:
+                y.append(float(sample['y'].iloc[rowIdx+1]))
 
     y = np.array(y)
     y_test = np.array(y_test)
@@ -133,9 +140,10 @@ if __name__ == '__main__':
     # data = data[data.clay<0.5]
     # test_data = test_data[test_data.clay<0.5]
     X1 = Input((len(input_attributes), max_len))
+    X2 = Input((len(input_attributes), max_len))
 
-    data = (x, y)
-    test_data = (x_test, y_test)
+    data = ((x, x2), y)
+    test_data = ((x_test, x2_test), y_test)
 
     hidden_units = 256
     num_cells = 1
@@ -145,14 +153,14 @@ if __name__ == '__main__':
         model = k.models.load_model('tennis_match_rnn.h5')
         model.compile(optimizer=Adam(lr=0.0001, decay=0.01), loss='binary_crossentropy', metrics=['accuracy'])
     else:
-
         norm = BatchNormalization()
-        model = norm(X1)
+        model = Concatenate(axis=1)([norm(X1), norm(X2)])
+
         for i in range(num_cells):
             model = LSTM(hidden_units, return_sequences=i != num_cells-1)(model)
 
         model = Dense(1, activation='sigmoid')(model)
-        model = Model(inputs=X1, outputs=model)
+        model = Model(inputs=[X1, X2], outputs=model)
         model.compile(optimizer=Adam(lr=0.0001, decay=0.01), loss='binary_crossentropy', metrics=['accuracy'])
 
     model_file = 'tennis_match_rnn.h5'
